fa1b6dbd13e999be0299d565a444d3ddcdff67e8
Rosen Santev <r.ssantev@gmail.com>
2024-09-19 15:53:40 +0300

    improve devnet docker config

--- h_devnet/scripts/run.sh
+++ h_devnet/scripts/run.sh
@@ -10,4 +10,4 @@ set -e
 ./genesis.sh
 
 # Execute specified hydra command with all arguments
-exec hydra server --data-dir ./node --chain genesis.json --grpc-address 127.0.0.1:9632 --libp2p 0.0.0.0:1478 --jsonrpc 0.0.0.0:8545 --seal --prometheus 0.0.0.0:5001 --log-level DEBUG --json-rpc-block-range-limit 0
+exec hydra "$@"
