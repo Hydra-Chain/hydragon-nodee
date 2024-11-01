84bc01527d378245237fe4472df58c0fdc7236ad
Vitomir Pavlov <vitomirpavlov@gmail.com>
2024-11-01 14:44:27 +0200

    make a separate variable for peerStatusCh in order to have consistent channel usage

--- syncer/client_test.go
+++ syncer/client_test.go
@@ -322,6 +322,7 @@ func TestPeerConnectionUpdateEventCh(t *testing.T) {
 	var (
 		wgForConnectingStatus sync.WaitGroup
 		newStatuses           []*NoForkPeer
+		statusCh              = client.GetPeerStatusUpdateCh()
 	)
 
 	wgForConnectingStatus.Add(1)
@@ -329,7 +330,7 @@ func TestPeerConnectionUpdateEventCh(t *testing.T) {
 	go func() {
 		defer wgForConnectingStatus.Done()
 
-		for status := range client.GetPeerStatusUpdateCh() {
+		for status := range statusCh {
 			newStatuses = append(newStatuses, status)
 		}
 	}()
